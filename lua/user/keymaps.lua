7f064c1084cea6d877f3de083b88a8c57f7ddfc8
daverussell13 <daverussell1309@gmail.com>
2022-01-06 09:59:48 +0700

    add clangd lsp server

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -27,6 +27,9 @@ keymap("n", "<C-l>", "<C-w>l", opts)
 
 keymap("n", "<leader>e", ":Lex 30<cr>", opts)
 
+-- turn off search highlight
+keymap("n", "<C-n>", ":nohl<CR>", opts)
+
 -- Resize with arrows
 keymap("n", "<C-Up>", ":resize -2<CR>", opts)
 keymap("n", "<C-Down>", ":resize +2<CR>", opts)
@@ -36,6 +39,8 @@ keymap("n", "<C-Right>", ":vertical resize +2<CR>", opts)
 -- Navigate buffers
 keymap("n", "<S-l>", ":bnext<CR>", opts)
 keymap("n", "<S-h>", ":bprevious<CR>", opts)
+-- Remove buffer
+keymap("n", "<S-x>", ":bd<CR>", opts)
 
 -- Move text up and down
 keymap("n", "<A-j>", "<Esc>:m .+1<CR>==gi", opts)
